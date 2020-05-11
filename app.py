4a2b82823f2d39e6663ad7187b5b49dd5bc3be90
Mannu Chauhan <Manojchauhan1395@hotmail.com>
2020-05-11 22:40:19 +0530

    Using Flask Migrate & Added a table todos .

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from os import sys, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+# Connecting Flask App  & SQLalchemy Database(POSTGRES)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sunny:123456@localhost:5432/testing'
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
-app = Flask(__name__)
-
-
-class Todo(db.Model){
+# Creating DataBase Table 'Todos'(Class) using sqlalchemy class Model Method
+class Todo(db.Model):
     __tablename__ = 'todos'
-    id = db.Column(db.Integer primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     #completed = db.Column(db.Boolean,nullable = False , default=False)
-}
+
+# Creating repr for the  table 'todos'.
 
 
 def __repr__(self):
-    return '<Todo: %s%s' >, ('self.id', 'self.description')
+    return '<Todo: %s%s>', ('self.id', 'self.description')
 
 
+# Creating controller for the  index page
 @app.route('/')
 def index():
     return render_template('index.html', todos=[{
