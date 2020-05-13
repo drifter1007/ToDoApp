2079fccbab8a386a31795a27d86282b689e59bde
Mannu Chauhan <Manojchauhan1395@hotmail.com>
2020-05-13 15:10:47 +0530

    0.1.4 Update Schema with changes in Migration
    Added completed column  while working with existing data

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    # completed = db.Column(db.Boolean,nullable = False , default=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
 
 # Creating repr for the  table 'todos'.
